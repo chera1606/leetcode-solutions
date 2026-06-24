953eddf9593aacb84fff1b771b2cbdc42dd8a920
Cherinet Derbie <cherinetderbie@gmail.com>
2026-06-24 22:31:25 +0300

    Time: 2 ms (48.17%) | Memory: 19.4 MB (25.32%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -1,7 +1,6 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         count = 0
-        
         for num in nums:
             if len(str(num)) % 2 == 0:
                 count += 1
